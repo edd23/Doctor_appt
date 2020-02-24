dc3e79058b1ff5d4ae5310b13f5fa171a2cbbabe
edd23 <yahired3@gmail.com>
2020-02-24 16:32:05 -0700

    user and doctor models finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
-  has_many :appointments
+  has_many :appointments, dependent: :destroy
   has_many :doctors, through: :appointments
+end
+
+def full_name
+  "#{self.first_name} #{self.last_name}"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'courses#index'
+
+  resources :users
+  resources :courses do
+  resources :appointments, only: [:index, :new, :create, :destroy]
+  end
 end
